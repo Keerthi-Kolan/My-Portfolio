97bbe4ce0ae63f8d599784d8c3f43908000496e8
Keerthi Kolan <kolankeerthi.reddy@gmail.com>
2024-01-09 20:45:03 -0800

    Adding all files

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Keerthi Kolan</h1>
+            <div className="links">
+                <a href="">About</a>
+                <a href="">Projects</a>
+                <a href="">Education</a>
+                <a href="">Contact</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
